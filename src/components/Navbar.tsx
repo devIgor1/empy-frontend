7e873a44a587bb3fa42aad4a6acfb630cb8f522d
devIgor1 <igormraesmm@gmail.com>
2025-07-15 13:16:39 -0300

    chore: integrate Tailwind CSS, update styles, and add Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import logo from "../assets/logo.svg"
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center px-4 py-2">
+      <button className="text-sm text-gray-700 border border-gray-300 rounded-full px-4 py-1 hover:bg-gray-100 cursor-pointer">
+        Voltar
+      </button>
+
+      <div className="mx-auto">
+        <img src={logo} alt="Empy Logo" className="h-6 md:h-8" />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
