69a7c20d040decd7150bbcaa990ec73cd92b3728
devIgor1 <igormraesmm@gmail.com>
2025-07-15 15:05:38 -0300

    style: adjust components position

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import HeroSection from "./components/HeroSection"
 import Navbar from "./components/Navbar"
 
 export default function PlansPage() {
   return (
-    <div>
+    <div className="max-w-6xl mx-auto">
       <Navbar />
+      <HeroSection />
     </div>
   )
 }
