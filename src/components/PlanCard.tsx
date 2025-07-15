9a82efdb334ccd7e55dfe1d03b26c9ee09cce1ee
devIgor1 <igormraesmm@gmail.com>
2025-07-15 17:58:22 -0300

    feat: refactor PlanCard and PlansToBuy components to utilize API data and improve props structure

--- src/components/PlanCard.tsx
+++ src/components/PlanCard.tsx
@@ -1,25 +1,24 @@
 import { PiCheckCircle } from "react-icons/pi"
-
-type PlanCardProps = {
-  title: string
-  priceAnual: string
-  priceMensal: string
-  beneficios: string[]
-}
+import type { Plan } from "../types"
+import { useNavigate } from "react-router-dom"
 
 const PlanCard = ({
-  title,
-  priceAnual,
-  priceMensal,
-  beneficios,
-}: PlanCardProps) => {
+  id,
+  publicName,
+  annualPrice,
+  monthlyPrice,
+  offlineCredits,
+  onlineCredits,
+}: Plan) => {
+  const navigate = useNavigate()
+
   return (
     <div className="empy-border-gray shadow-sm flex flex-col justify-between p-[10px]">
       <div>
         <h3 className="text-lg">Plano</h3>
-        <span className="text-black font-bold text-2xl">{title}</span>
+        <span className="text-black font-bold text-2xl">{publicName}</span>
 
-        <div className="empy-border-blue p-1 my-3 text-center p-[5px]">
+        <div className="empy-border-blue my-3 text-center p-[5px]">
           <p className="empy-text-blue text-md font-medium w-full">
             Ganhe 2 meses de desconto na contratação anual com 12 meses de
             fidelidade
@@ -27,16 +26,18 @@ const PlanCard = ({
           <p className="empy-text-gray font-bold flex items-center justify-center gap-1">
             Anual R$
             <span className="empy-text-blue font-bold text-4xl">
-              {priceAnual}
+              {(annualPrice / 12).toFixed(2)}
             </span>
             / mensais
           </p>
         </div>
 
-        <div className="">
+        <div>
           <p className="empy-text-gray font-bold flex items-center justify-center gap-1">
             Mensal R$
-            <span className="text-black font-bold text-4xl">{priceMensal}</span>
+            <span className="text-black font-bold text-4xl">
+              {monthlyPrice.toFixed(2)}
+            </span>
             / mensais
           </p>
         </div>
@@ -46,21 +47,33 @@ const PlanCard = ({
             <PiCheckCircle size={20} color="white" />
             Consulta de benefícios do INSS
           </p>
-          {beneficios.map((b, i) => (
-            <p key={i} className="text-sm flex items-center gap-2">
-              <span
-                className={`h-2 w-2 rounded-full ${
-                  b.includes("ILIMITADOS") ? "bg-red-500" : "bg-green-500"
-                }`}
-              />
-              {b}
-            </p>
-          ))}
+
+          <p className="text-sm flex items-center gap-2">
+            <span className="h-2 w-2 rounded-full bg-green-500" />
+            {offlineCredits} créditos offline
+          </p>
+          <p className="text-sm flex items-center gap-2">
+            <span
+              className={`h-2 w-2 rounded-full ${
+                onlineCredits.includes("ILIMITADOS")
+                  ? "bg-red-500"
+                  : "bg-green-500"
+              }`}
+            />
+            {onlineCredits} créditos online
+          </p>
+
           <div className="flex flex-col gap-2 mt-4">
-            <button className="empy-button-blue hover:bg-[#006EB6] transition-all duration-300 hover:text-white">
+            <button
+              className="empy-button-blue hover:bg-[#006EB6]"
+              onClick={() => navigate(`/checkout/anual?planId=${id}`)}
+            >
               Assinar anual
             </button>
-            <button className="empy-button-blue hover:bg-[#006EB6] transition-all duration-300 hover:text-white">
+            <button
+              className="empy-button-blue hover:bg-[#006EB6]"
+              onClick={() => navigate(`/checkout/mensal?planId=${id}`)}
+            >
               Assinar mensal
             </button>
           </div>
--- src/components/PlansToBuy.tsx
+++ src/components/PlansToBuy.tsx
@@ -1,34 +1,45 @@
 import PlanCard from "./PlanCard"
 import { PiWhatsappLogo } from "react-icons/pi"
+import { api } from "../lib/axios"
+import { useEffect, useState } from "react"
+import type { Plan } from "../types"
 
 const Plans = () => {
+  const [plans, setPlans] = useState<Plan[]>([])
+
+  useEffect(() => {
+    api.get("/plans").then((response) => {
+      setPlans(response.data)
+    })
+  }, [])
+
   return (
     <section className="max-w-7xl mx-auto px-4 py-10">
       <h2 className="text-xl font-bold mb-6">Planos de acesso</h2>
 
       <div className="grid gap-6 grid-cols-1 md:grid-cols-3">
-        <PlanCard
-          title="Light"
-          priceAnual="130,83"
-          priceMensal="157,00"
-          beneficios={["2 Créditos Offline", "20 Créditos Offline"]}
-        />
-        <PlanCard
-          title="Standard"
-          priceAnual="207,91"
-          priceMensal="249,50"
-          beneficios={["10 Créditos Offline", "30 Créditos Offline"]}
-        />
-        <PlanCard
-          title="Pro"
-          priceAnual="289,92"
-          priceMensal="347,00"
-          beneficios={["30 Créditos Offline", "Créditos Offline ILIMITADOS"]}
-        />
+        {plans.map((plan) => (
+          <PlanCard
+            createdAt={plan.createdAt}
+            key={plan.id}
+            id={plan.id}
+            publicName={plan.publicName}
+            internalName={plan.internalName}
+            monthlyPrice={plan.monthlyPrice}
+            annualPrice={plan.annualPrice}
+            discount={plan.discount}
+            isCustom={plan.isCustom}
+            offlineCredits={plan.offlineCredits}
+            onlineCredits={plan.onlineCredits}
+            isActive={plan.isActive}
+            isRecommended={plan.isRecommended}
+            paymentLink={plan.paymentLink}
+          />
+        ))}
       </div>
 
       <div className="mt-8 flex justify-start">
-        <div className="bg-[#5E17F5] text-white px-5 py-3 rounded-full text-sm flex items-center justify-center gap-2">
+        <div className="bg-[#5E17F5] text-white px-5 py-3 rounded-full text-sm flex items-center justify-center gap-2 cursor-pointer hover:bg-[#4c0ed1] transition">
           <PiWhatsappLogo size={17} color="white" />
           <span className="text-sm">Tire suas dúvidas</span>
         </div>
