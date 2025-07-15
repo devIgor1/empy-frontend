ed7cba19f7dc3b7fa4e066920cd72ba0f2bbbc70
devIgor1 <igormraesmm@gmail.com>
2025-07-15 15:05:50 -0300

    feat: finish HeroSection

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,25 @@
+import awnings from "../assets/headerAwnings.svg"
+import empyPlan from "../assets/empyPlan.svg"
+import Plans from "./Plans"
+
+const HeroSection = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-between max-w-6xl mx-auto bg-white rounded-lg h-full">
+        <div className="flex justify-center flex-col items-center p-2">
+          <div className="flex">
+            {Array.from({ length: 4 }).map((_, i) => (
+              <img key={i} src={awnings} alt={`awning-${i}`} />
+            ))}
+          </div>
+          <div className="flex flex-col items-center justify-center mt-5">
+            <img src={empyPlan} alt="empyPlan" className="w-full h-auto" />
+          </div>
+          <Plans />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default HeroSection
