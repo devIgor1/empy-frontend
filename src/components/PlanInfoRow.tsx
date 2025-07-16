d017a4a890344e8ff24341da1780883e7a1c2442
devIgor1 <igormraesmm@gmail.com>
2025-07-16 13:52:51 -0300

    feat: create PlanInfoRow component for displaying plan details

---
+++ src/components/PlanInfoRow.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+
+interface PlanInfoRowProps {
+  label: string
+  value: string | React.ReactNode
+  rightContent?: React.ReactNode
+  className?: string
+  showTopSpacing?: boolean
+}
+
+const PlanInfoRow: React.FC<PlanInfoRowProps> = ({
+  label,
+  value,
+  rightContent,
+  className = "",
+  showTopSpacing = false,
+}) => {
+  return (
+    <div className={`${className}`}>
+      <div
+        className={`flex justify-between items-center ${
+          rightContent ? "" : "justify-start"
+        }`}
+      >
+        <p className="text-sm">{label}</p>
+
+        {rightContent && (
+          <div className="flex-shrink-0 ml-2">{rightContent}</div>
+        )}
+      </div>
+
+      <div className={`${showTopSpacing ? "mt-2 sm:mt-3" : "mt-1"}`}>
+        <p className="text-sm break-words">{value}</p>
+      </div>
+    </div>
+  )
+}
+
+export default PlanInfoRow
