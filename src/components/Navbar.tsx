0e68ddb5b4043d9b1635e438ae72e6a76298febc
devIgor1 <igormraesmm@gmail.com>
2025-07-17 15:53:05 -0300

    feat: add navigation functionality to Navbar component with back button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,14 @@
 import logo from "../assets/logo.svg"
+import { useNavigate } from "react-router-dom"
 
 const Navbar = () => {
+  const navigate = useNavigate()
   return (
     <nav className="flex justify-center items-center px-4 py-2 w-full mx-auto">
-      <button className="text-sm text-gray-700 border border-gray-300 rounded-full px-4 py-1 hover:bg-gray-100 cursor-pointer">
+      <button
+        className="text-sm text-gray-700 border border-gray-300 rounded-full px-4 py-1 hover:bg-gray-100 cursor-pointer"
+        onClick={() => navigate(-1)}
+      >
         Voltar
       </button>
 
